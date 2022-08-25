7620de21e829b65d15e48ece280d4e5cf1c01a73
Scassanl <Scassanl0511@gmai.com>
2022-08-25 20:08:37 +0800

    Minio的抽取和自动配置

--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/FileuploadController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/FileuploadController.java
@@ -2,19 +2,34 @@ package com.atguigu.gmall.product.controller;
 
 
 import com.atguigu.gmall.common.result.Result;
+import com.atguigu.gmall.product.service.FileUploadService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * 文件上传Controller
+ */
 @RequestMapping("/admin/product")
 @RestController
 public class FileuploadController {
 
+    @Autowired
+    FileUploadService fileUploadService;
 
+    /**
+     * 后台品牌图片上传
+     * @param file
+     * @return
+     * @throws Exception
+     */
     @PostMapping("/fileUpload")
-    public Result fileUpload(){
-
-        return Result.ok();
+    public Result fileUpload(@RequestPart("file")MultipartFile file) throws Exception {
+        String url = fileUploadService.upload(file);
+        return Result.ok(url);
     }
 
 }
